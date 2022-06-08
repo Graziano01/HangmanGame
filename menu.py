1b4591749bf015e14357fb12dc39c8abc4f0717a
Graziano01 <graziano01castiglione@gmail.com>
2022-06-08 21:55:44 +0200

    menu v0.1

---
+++ menu.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+root.title('Menù v0.1')
+root.geometry('1600x900')
+
+lab = Label(root, text='The Hangman Game!', font=('Georgia', 40))
+lab.pack(side='top')
+
+# Start button
+start_button = Button(root, text='Start', font=('Georgia', 15), width=10, height=3) # todo: add command function
+start_button.pack(side='right', padx=10, pady=10)
+
+root.mainloop()
