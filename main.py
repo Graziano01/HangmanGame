351e1309d4943411bd1bce6e6fb38f6ba7e77d7b
Graziano <graziano@pop-os.localdomain>
2022-09-02 12:54:27 +0200

    feat: add play and stats button

--- main.py
+++ main.py
@@ -15,24 +15,19 @@ def mainMenu() -> None:                                             # Funzione p
 
         MOUSE_POS = pygame.mouse.get_pos()                          # Posizione del mouse
 
-        QUIT_BUT = Button(image=pygame.image.load("quit.png"),      # Bottone per uscire dal gioco
-                    pos=(640, 460), text_input="QUIT", font=FONT, 
+        PLAY_BUT = Button(image=pygame.image.load("rect.png"),      # Bottone per giocare
+                    pos=(1050, 80), text_input="PLAY", font=FONT,
                     base_color="black", hovering_color="red")
 
-        TESTO_MENU1 = FONT.render("PROVA 1", True, "black")         # Testo 1
-        TESTO_CENT1 = TESTO_MENU1.get_rect(center=(1000, 80))       # Posizione testo 1
-        
-        TESTO_MENU2 = FONT.render("PROVA 2", True, "black")         # Testo 2
-        TESTO_CENT2 = TESTO_MENU2.get_rect(center=(1000, 170))      # Posizione testo 2
-        
-        TESTO_MENU3 = FONT.render("PROVA 3", True, "black")         # Testo 3
-        TESTO_CENT3 = TESTO_MENU3.get_rect(center=(1000, 250))      # Posizione testo 3
-        
-        SCREEN.blit(TESTO_MENU1, TESTO_CENT1)                       # Colloca testo 1 nello schermo
-        SCREEN.blit(TESTO_MENU2, TESTO_CENT2)                       # Colloca testo 2 nello schermo
-        SCREEN.blit(TESTO_MENU3, TESTO_CENT3)                       # Colloca testo 3 nello schermo
+        STATS_BUT = Button(image=pygame.image.load("rect.png"),     # Bottone per le statistiche
+                    pos=(1050, 190), text_input="STATS", font=FONT,
+                    base_color="black", hovering_color="red")
 
-        for button in [QUIT_BUT]:                                   
+        QUIT_BUT = Button(image=pygame.image.load("rect.png"),      # Bottone per uscire dal gioco
+                    pos=(1050, 300), text_input="QUIT", font=FONT, 
+                    base_color="black", hovering_color="red")
+
+        for button in [PLAY_BUT, STATS_BUT, QUIT_BUT]:                                   
             button.changeColor(MOUSE_POS)                           # Cambia il colore del bottone
             button.update(SCREEN)                                   # Aggiorna il bottone
 
