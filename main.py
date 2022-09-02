0ccff5be44d7fc2ecb907d927856ef6eeeb2ac24
Graziano <graziano@pop-os.localdomain>
2022-09-02 13:16:27 +0200

    feat: add play and stats screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+import mysql.connector
 from button import Button
 
 pygame.init()                                                       # Inizializza la finestra
@@ -8,6 +9,64 @@ pygame.display.set_caption("Hangman Game")                          # Titolo del
 
 BACKGROUND = pygame.image.load("img.png")                           # Carica il background
 FONT = pygame.font.SysFont("Arial", 80, bold=True)                  # Imposta il font
+
+
+
+def playMenu() -> None:                                             # Funzione per il Menu di Gioco
+    while True:
+        SCREEN.fill("black")
+
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        PLAY_TEXT = FONT.render("PLAY SCREEN", True, "white")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=FONT, 
+                            base_color="white", 
+                            hovering_color="green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    mainMenu()
+
+        pygame.display.update()
+
+def statsMenu() -> None:                                            # Funzione per il Menu delle Statistiche
+    while True:
+        SCREEN.fill("black")
+
+        STATS_MOUSE_POS = pygame.mouse.get_pos()
+
+        STATS_TEXT = FONT.render("STATS SCREEN", True, "white")
+        STATS_RECT = STATS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(STATS_TEXT, STATS_RECT)
+
+        STATS_BACK = Button(image=None, pos=(640, 460),
+                            text_input="BACK", font=FONT,
+                            base_color="white", 
+                            hovering_color="green")
+
+        STATS_BACK.changeColor(STATS_MOUSE_POS)
+        STATS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if STATS_BACK.checkForInput(STATS_MOUSE_POS):
+                    mainMenu()
+        
+        pygame.display.update()
 
 def mainMenu() -> None:                                             # Funzione per il Menu Principale
     while True:
@@ -39,6 +98,10 @@ def mainMenu() -> None:                                             # Funzione p
                 if QUIT_BUT.checkForInput(MOUSE_POS):               
                     pygame.quit()
                     sys.exit()                                      # Se si preme il bottone QUIT chiude la finestra
+                if STATS_BUT.checkForInput(MOUSE_POS):
+                    statsMenu()                                     # Se si preme il bottone STATS apre il menu delle statistiche
+                if PLAY_BUT.checkForInput(MOUSE_POS):
+                    playMenu()                                      # Se si preme il bottone PLAY apre il menu di gioco
 
         pygame.display.update()                                     # Aggiorna lo schermo
 
