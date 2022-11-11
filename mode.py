06d773528aa47f455fc76bf8a32a098d596eaf0b
Graziano <graziano01castiglione@gmail.com>
2022-11-11 14:09:51 +0100

    refactoring: moved ita-eng functions in one function

--- mode.py
+++ mode.py
@@ -1,73 +1,73 @@
 import random
 
-def fast_eng() -> str:
-    words = []
-    with open("assets/wordlistengCompleto.txt", "r") as f:
-        for line in f:
-            word = line.rstrip("\n")
-            words.append(word)
-        rand_word = random.choice(words)
-    return rand_word
+def fast(lang: str) -> str:
+    if lang == "eng":
+        words = []
+        with open("assets/wordlistengCompleto.txt", "r") as f:
+            for line in f:
+                word = line.rstrip("\n")
+                words.append(word)
+            rand_word = random.choice(words)
+        return rand_word
+    elif lang == "ita":
+        words = []
+        with open("assets/wordlistitaCompleto.txt", "r") as f:
+            for line in f:
+                word = line.rstrip("\n")
+                words.append(word)
+            rand_word = random.choice(words)
+        return rand_word
 
-def get_word_easy_eng() -> str:
-    words = []
-    with open("assets/wordlistengCorte.txt", "r") as f:
-        for line in f:
-            word = line.rstrip("\n")
-            words.append(word)
-        rand_word = random.choice(words)
-    return rand_word
+def get_word_easy(lang: str) -> str:
+    if lang == "eng":
+        words = []
+        with open("assets/wordlistengCorte.txt", "r") as f:
+            for line in f:
+                word = line.rstrip("\n")
+                words.append(word)
+            rand_word = random.choice(words)
+        return rand_word
+    elif lang == "ita":
+        words = []
+        with open("assets/wordlistitaCorte.txt", "r") as f:
+            for line in f:
+                word = line.rstrip("\n")
+                words.append(word)
+            rand_word = random.choice(words)
+        return rand_word
 
-def get_word_med_eng() -> str:
-    words = []
-    with open("assets/wordlistengMedie.txt", "r") as f:
-        for line in f:
-            word = line.rstrip("\n")
-            words.append(word)
-        rand_word = random.choice(words)
-    return rand_word
+def get_word_med(lang: str) -> str:
+    if lang == "eng":
+        words = []
+        with open("assets/wordlistengMedie.txt", "r") as f:
+            for line in f:
+                word = line.rstrip("\n")
+                words.append(word)
+            rand_word = random.choice(words)
+        return rand_word
+    elif lang == "ita":
+        words = []
+        with open("assets/wordlistitaMedie.txt", "r") as f:
+            for line in f:
+                word = line.rstrip("\n")
+                words.append(word)
+            rand_word = random.choice(words)
+        return rand_word
 
-def get_word_hard_eng() -> str:
-    words = []
-    with open("assets/wordlistengLunghe.txt", "r") as f:
-        for line in f:
-            word = line.rstrip("\n")
-            words.append(word)
-        rand_word = random.choice(words)
-    return rand_word
-
-def fast_ita() -> str:
-    words = []
-    with open("assets/wordlistitaCompleto.txt", "r") as f:
-        for line in f:
-            word = line.rstrip("\n")
-            words.append(word)
-        rand_word = random.choice(words)
-    return rand_word
-
-def get_word_easy_ita() -> str:
-    words = []
-    with open("assets/wordlistitaCorte.txt", "r") as f:
-        for line in f:
-            word = line.rstrip("\n")
-            words.append(word)
-        rand_word = random.choice(words)
-    return rand_word
-
-def get_word_med_ita() -> str:
-    words = []
-    with open("assets/wordlistitaMedie.txt", "r") as f:
-        for line in f:
-            word = line.rstrip("\n")
-            words.append(word)
-        rand_word = random.choice(words)
-    return rand_word
-
-def get_word_hard_ita() -> str:
-    words = []
-    with open("assets/wordlistitaLunghe.txt", "r") as f:
-        for line in f:
-            word = line.rstrip("\n")
-            words.append(word)
-        rand_word = random.choice(words)
-    return rand_word
+def get_word_hard(lang: str) -> str:
+    if lang == "eng":
+        words = []
+        with open("assets/wordlistengLunghe.txt", "r") as f:
+            for line in f:
+                word = line.rstrip("\n")
+                words.append(word)
+            rand_word = random.choice(words)
+        return rand_word
+    elif lang == "ita":
+        words = []
+        with open("assets/wordlistitaLunghe.txt", "r") as f:
+            for line in f:
+                word = line.rstrip("\n")
+                words.append(word)
+            rand_word = random.choice(words)
+        return rand_word
